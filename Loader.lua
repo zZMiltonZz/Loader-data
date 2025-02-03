09bfc4fe9d6632b051301c47f0d1d5ec3b82b1ec
Milton <91810223+zZMiltonZz@users.noreply.github.com>
2025-02-03 03:41:13 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,7 @@ key_user_map = {
     LiveKey1 = { user = "B1GB0Y", tier = "LIVE" },
     BetaKey1 = { user = "Alice", tier = "BETA" },
     VipKey1 = { user = "Bob", tier = "VIP" },
+    VipKey2 = { user = "Milton.XD", tier = "VIP" },
     SecretKey1 = { user = "Milton.XD", tier = "SECRET" },
     LiveKey2 = { user = "John", tier = "LIVE" }
 }
